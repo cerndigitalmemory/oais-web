96d49a2de86a9d526e34032841c637b62cb6970a
Marco Donadoni <marco.donadoni@cern.ch>
2021-07-23 11:04:00 +0200

    Show a spinner while making a search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,34 +3,37 @@ import { RecordsList } from "@/components/RecordsList.jsx";
 import { sendNotification } from "@/utils.js";
 import PropTypes from "prop-types";
 import React from "react";
-import { Button, Col, Form, Row } from "react-bootstrap";
+import { Button, Col, Form, Row, Spinner } from "react-bootstrap";
 
 export class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      results: [],
-    };
-  }
+  state = {
+    results: [],
+    isLoading: false,
+  };
 
   handleSearch = async (source, query) => {
+    this.setState({ isLoading: true });
     try {
       const results = await api.search(source, query);
       this.setState({ results });
     } catch (e) {
       sendNotification("Error while searching", e.message);
+    } finally {
+      this.setState({ isLoading: false });
     }
   };
 
   render() {
+    const { isLoading, results } = this.state;
     return (
       <React.Fragment>
         <h1>Search</h1>
         <SearchForm
           sources={["cds-test", "cds"]}
           onSearch={this.handleSearch}
+          isLoading={isLoading}
         />
-        <RecordsList records={this.state.results} />
+        <RecordsList records={results} />
       </React.Fragment>
     );
   }
@@ -40,6 +43,7 @@ export class SearchForm extends React.Component {
   static propTypes = {
     sources: PropTypes.arrayOf(PropTypes.string).isRequired,
     onSearch: PropTypes.func.isRequired,
+    isLoading: PropTypes.bool.isRequired,
   };
 
   constructor(props) {
@@ -64,6 +68,20 @@ export class SearchForm extends React.Component {
   };
 
   render() {
+    const { isLoading } = this.props;
+
+    let submitButton;
+    if (isLoading) {
+      // if the search is already in progress, show a spinner
+      submitButton = (
+        <Button type="submit" disabled>
+          <Spinner as="span" animation="border" size="sm" role="status" />
+        </Button>
+      );
+    } else {
+      submitButton = <Button type="submit">Search</Button>;
+    }
+
     return (
       <Form onSubmit={this.handleSubmit} className="mb-3">
         <Row>
@@ -103,7 +121,7 @@ export class SearchForm extends React.Component {
           </Col>
 
           <Col xs="12" lg="2" className="text-center">
-            <Button type="submit">Search</Button>
+            {submitButton}
           </Col>
         </Row>
       </Form>
