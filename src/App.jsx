ee9508c8d159a728703ad869c5f0426b0d0cee7c
Kostas Chelakis <konstantinos.chelakis@cern.ch>
2022-03-23 13:42:34 +0100

    Reorganize layout and fix responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,7 @@
 import { AppContext } from '@/AppContext.js'
 import { NavigationBar } from '@/components/Navigation/NavigationBar.jsx'
-import { Notifications } from '@/components/Notifications/Notifications.jsx'
-import { ProtectedRoute } from '@/components/ProtectedRoute/ProtectedRoute.jsx'
-import { Home } from '@/pages/Home/Home.jsx'
-import { Archives } from '@/pages/Archives/Archives.jsx'
-import { EditManifests } from '@/pages/EditManifests/EditManifests.jsx'
-import { Search } from '@/pages/Search/Search.jsx'
-import { ArchiveSteps } from '@/pages/ArchiveDetail/ArchiveSteps.jsx'
-import { CollectionDetails } from '@/pages/CollectionDetailsPage/CollectionDetails.jsx'
-import { Login } from '@/pages/Login/Login.jsx'
-import { LoginCallback } from '@/components/AuthenticationService/LoginCallback.jsx'
-import { Logout } from '@/components/AuthenticationService/Logout.jsx'
-import { RecordDetail } from '@/pages/Detail/RecordDetail.jsx'
-import { SIPDetailPage } from '@/pages/Detail/SIPDetailPage.jsx'
-import { Settings } from '@/pages/Settings/Settings.jsx'
-import { StagedRecords } from '@/pages/StagedRecords/StagedRecords.jsx'
-import Harvest from '@/pages/Harvest/Harvest.jsx'
-import { UserDetail } from '@/pages/Detail/UserDetail.jsx'
-import { Actions } from '@/pages/Actions/Actions.jsx'
-import { AddResource } from '@/pages/AddResource/AddResource.jsx'
-import { Upload } from '@/pages/Upload/Upload.jsx'
-import { Collections } from '@/pages/Collections/Collections.jsx'
 import React from 'react'
-import { Container } from 'semantic-ui-react'
-import { Route, Switch } from 'react-router-dom'
+
 
 export class App extends React.Component {
   constructor(props) {
@@ -37,42 +15,12 @@ export class App extends React.Component {
 
   render() {
     const { notifications } = this.state
+
     return (
       <AppContext.Context.Provider value={this.state}>
         <div id="app">
-          <NavigationBar />
-          <Notifications notifications={notifications ?? []} />
-          <Container>
-            <Switch>
-              <ProtectedRoute path="/harvest" component={Harvest} />
-              <ProtectedRoute path="/add-resource" component={AddResource} />
-              <ProtectedRoute path="/archives" component={Archives} />
-              <ProtectedRoute path="/search/" component={Search} />
-              <ProtectedRoute path="/archive/:id" component={ArchiveSteps} />
-              <ProtectedRoute
-                path="/edit-archive/:id"
-                component={EditManifests}
-              />
-              <ProtectedRoute
-                path="/collection/:id"
-                component={CollectionDetails}
-              />
-              <ProtectedRoute path="/records/:id" component={RecordDetail} />
-              <ProtectedRoute path="/details/:id" component={SIPDetailPage} />
-              <ProtectedRoute path="/users/:id" component={UserDetail} />
-              <ProtectedRoute path="/upload" component={Upload} />
-              <ProtectedRoute path="/staged" component={StagedRecords} />
-              <ProtectedRoute path="/actions" component={Actions} />
-              <ProtectedRoute path="/collections" component={Collections} />
-              <ProtectedRoute path="/settings" component={Settings} />
-              <Route path="/login/callback" component={LoginCallback} />
-              <Route path="/logout" component={Logout} />
-              <Route path="/login" component={Login} />
-              <Route path="/">
-                <Home />
-              </Route>
-            </Switch>
-          </Container>
+          <NavigationBar  notifications = {notifications}/>
+   
         </div>
       </AppContext.Context.Provider>
     )
--- src/components/Navigation/NavigationBar.jsx
+++ src/components/Navigation/NavigationBar.jsx
@@ -1,14 +1,56 @@
 import { AppContext } from '@/AppContext.js'
-import React from 'react'
+import { createMedia } from '@artsy/fresnel'
+import PropTypes, { object } from 'prop-types'
+import { userType, notificationType } from '@/types.js'
 import { Link } from 'react-router-dom'
-import { Menu, Icon } from 'semantic-ui-react'
+import { Menu, Icon, Sidebar, Segment, Loader } from 'semantic-ui-react'
+import { Notifications } from '@/components/Notifications/Notifications.jsx'
+import { ProtectedRoute } from '@/components/ProtectedRoute/ProtectedRoute.jsx'
+import { Home } from '@/pages/Home/Home.jsx'
+import { Archives } from '@/pages/Archives/Archives.jsx'
+import { EditManifests } from '@/pages/EditManifests/EditManifests.jsx'
+import { Search } from '@/pages/Search/Search.jsx'
+import { ArchiveSteps } from '@/pages/ArchiveDetail/ArchiveSteps.jsx'
+import { CollectionDetails } from '@/pages/CollectionDetailsPage/CollectionDetails.jsx'
+import { Login } from '@/pages/Login/Login.jsx'
+import { LoginCallback } from '@/components/AuthenticationService/LoginCallback.jsx'
+import { Logout } from '@/components/AuthenticationService/Logout.jsx'
+import { RecordDetail } from '@/pages/Detail/RecordDetail.jsx'
+import { SIPDetailPage } from '@/pages/Detail/SIPDetailPage.jsx'
+import { Settings } from '@/pages/Settings/Settings.jsx'
+import { StagedRecords } from '@/pages/StagedRecords/StagedRecords.jsx'
+import Harvest from '@/pages/Harvest/Harvest.jsx'
+import { UserDetail } from '@/pages/Detail/UserDetail.jsx'
+import { Actions } from '@/pages/Actions/Actions.jsx'
+import { AddResource } from '@/pages/AddResource/AddResource.jsx'
+import { Upload } from '@/pages/Upload/Upload.jsx'
+import { Collections } from '@/pages/Collections/Collections.jsx'
+import React from 'react'
+import { Container } from 'semantic-ui-react'
+import { Route, Switch } from 'react-router-dom'
 
 /**
  * Renders the Navigation Bar, keeping track of the active page and
  * showing some links only to authenticated users.
  */
+const AppMedia = createMedia({
+  breakpoints: {
+    mobile: 320,
+    tablet: 768,
+    computer: 992,
+    largeScreen: 1200,
+    widescreen: 1920,
+  },
+})
+
+const mediaStyles = AppMedia.createMediaStyle()
+const { Media, MediaContextProvider } = AppMedia
+
 export class NavigationBar extends React.Component {
   static contextType = AppContext.Context
+  static propTypes = {
+    notifications: notificationType,
+  }
 
   constructor(props) {
     super(props)
@@ -88,23 +130,193 @@ export class NavigationBar extends React.Component {
 
   render() {
     const { isLoggedIn, user } = this.context
+    const { links, activeItem } = this.state
+    let showNavbar = <Loader />
+    if (user && isLoggedIn) {
+      showNavbar = (
+        <NavBar
+          leftItems={links}
+          isLoggedIn={isLoggedIn}
+          user={user}
+          activeItem={activeItem}
+          notifications={this.props.notifications}
+        ></NavBar>
+      )
+    }
+
+    return (
+      <>
+        <style>{mediaStyles}</style>
+
+        <MediaContextProvider>{showNavbar}</MediaContextProvider>
+      </>
+    )
+  }
+}
+
+class NavBar extends React.Component {
+  static propTypes = {
+    leftItems: PropTypes.arrayOf(object).isRequired,
+    isLoggedIn: PropTypes.bool.isRequired,
+    user: userType.isRequired,
+    activeItem: PropTypes.string,
+    notifications: notificationType,
+  }
+  state = {
+    visible: false,
+  }
+
+  handlePusher = () => {
+    const { visible } = this.state
+
+    if (visible) this.setState({ visible: false })
+  }
+
+  handleToggle = () => this.setState({ visible: !this.state.visible })
+
+  render() {
+    const { leftItems, isLoggedIn, user, activeItem, notifications } = this.props
+    const { visible } = this.state
+
+    return (
+      <div>
+        <Media at="mobile">
+          <NavBarMobile
+            leftItems={leftItems}
+            onPusherClick={this.handlePusher}
+            onToggle={this.handleToggle}
+            visible={visible}
+            isLoggedIn={isLoggedIn}
+            user={user}
+            activeItem={activeItem}
+          ></NavBarMobile>
+        </Media>
+
+        <Media greaterThan="mobile">
+          <NavBarDesktop
+            leftItems={leftItems}
+            isLoggedIn={isLoggedIn}
+            user={user}
+            activeItem={activeItem}
+            
+          />
+          <NavBarChildren notifications= {notifications}/>
+        </Media>
+      </div>
+    )
+  }
+}
+
+class NavBarMobile extends React.Component {
+  static propTypes = {
+    leftItems: PropTypes.array.isRequired,
+    onPusherClick: PropTypes.func,
+    onToggle: PropTypes.func,
+    visible: PropTypes.bool.isRequired,
+    isLoggedIn: PropTypes.bool.isRequired,
+    user: userType.isRequired,
+    activeItem: PropTypes.string,
+  }
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const {
+      leftItems,
+      onPusherClick,
+      onToggle,
+      visible,
+      isLoggedIn,
+      user,
+      activeItem,
+      notifications
+    } = this.props
+
+    return (
+      <Sidebar.Pushable style={{ minHeight: '100%' }}>
+        <Sidebar
+          as={Menu}
+          animation="overlay"
+          icon="labeled"
+          inverted
+          vertical
+          visible={visible}
+          width={'wide'}
+          style={{ position: 'absolute' }}
+        >
+          {leftItems
+            .filter((link) => link.always || link.loggedIn === isLoggedIn)
+            .map((link) => (
+              <Menu.Item
+                key={link.key}
+                as={Link}
+                active={link.key == activeItem}
+                to={link.to}
+                onClick={this.handleItemClick}
+                name={link.key}
+              >
+                {link.value}
+              </Menu.Item>
+            ))}
+        </Sidebar>
+        <Sidebar.Pusher onClick={onPusherClick} style={{ minHeight: '100vh' }}>
+          <Menu secondary>
+            <Menu.Item onClick={onToggle}>
+              <Icon name="sidebar" />
+            </Menu.Item>
+            {isLoggedIn && (
+              <Menu.Menu position="right">
+                <Menu.Item>
+                  Hello,&nbsp;{' '}
+                  <Link to={`/users/${user.id}`}>{user.username}</Link>
+                </Menu.Item>
+                <Menu.Item as={Link} to="/settings">
+                  <Icon name="settings" />
+                </Menu.Item>
+              </Menu.Menu>
+            )}
+          </Menu>
+          <NavBarChildren notifications= {notifications}/>
+          
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
+    )
+  }
+}
+
+class NavBarDesktop extends React.Component {
+  static propTypes = {
+    leftItems: PropTypes.array.isRequired,
+    isLoggedIn: PropTypes.bool.isRequired,
+    user: userType.isRequired,
+    activeItem: PropTypes.string,
+  }
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const { leftItems, isLoggedIn, user, activeItem } = this.props
+    console.log(activeItem)
 
     return (
       <Menu secondary>
-        {this.state.links
+        {leftItems
           .filter((link) => link.always || link.loggedIn === isLoggedIn)
           .map((link) => (
             <Menu.Item
               key={link.key}
               as={Link}
+              active={link.key == activeItem}
               to={link.to}
-              active={link.key == this.state.activeItem}
               onClick={this.handleItemClick}
               name={link.key}
             >
               {link.value}
             </Menu.Item>
           ))}
+
         {isLoggedIn && (
           <Menu.Menu position="right">
             <Menu.Item>
@@ -116,6 +328,56 @@ export class NavigationBar extends React.Component {
           </Menu.Menu>
         )}
       </Menu>
+    )
+  }
+}
+
+class NavBarChildren extends React.Component {
+  static propTypes = {
+    notifications: notificationType,
+  }
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const { notifications } = this.props
+
+    return (
+      <React.Fragment>
+        <Notifications notifications={notifications ?? []} />
+          <Container>
+            <Switch>
+              <ProtectedRoute path="/harvest" component={Harvest} />
+              <ProtectedRoute path="/add-resource" component={AddResource} />
+              <ProtectedRoute path="/archives" component={Archives} />
+              <ProtectedRoute path="/search/" component={Search} />
+              <ProtectedRoute path="/archive/:id" component={ArchiveSteps} />
+              <ProtectedRoute
+                path="/edit-archive/:id"
+                component={EditManifests}
+              />
+              <ProtectedRoute
+                path="/collection/:id"
+                component={CollectionDetails}
+              />
+              <ProtectedRoute path="/records/:id" component={RecordDetail} />
+              <ProtectedRoute path="/details/:id" component={SIPDetailPage} />
+              <ProtectedRoute path="/users/:id" component={UserDetail} />
+              <ProtectedRoute path="/upload" component={Upload} />
+              <ProtectedRoute path="/staged" component={StagedRecords} />
+              <ProtectedRoute path="/actions" component={Actions} />
+              <ProtectedRoute path="/collections" component={Collections} />
+              <ProtectedRoute path="/settings" component={Settings} />
+              <Route path="/login/callback" component={LoginCallback} />
+              <Route path="/logout" component={Logout} />
+              <Route path="/login" component={Login} />
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Container>
+      </React.Fragment>
     )
   }
 }
