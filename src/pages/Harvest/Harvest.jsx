72a2d9306c3ea59c8b0f47505f6c1b61d21c5568
Manuel Salomon <manuel.salomon@cern.ch>
2022-06-16 16:25:32 +0200

    Added codimd in dropdown as source

--- src/pages/Harvest/Harvest.jsx
+++ src/pages/Harvest/Harvest.jsx
@@ -218,7 +218,7 @@ class Harvest extends React.Component {
           Bagit Create tool)
         </p>
         <SearchForm
-          sources={['cds', 'zenodo', 'inveniordm', 'cod', 'indico']}
+          sources={['cds', 'zenodo', 'inveniordm', 'cod', 'indico', 'codimd']}
           activeSource={this.props.source}
           onSearch={this.handleSearch}
           isLoading={isLoading}
